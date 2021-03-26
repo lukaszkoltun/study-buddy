aa92097bb1cf20a8ace9177740e6c29960e9c885
lukasz.koltun <lukasz.koltun@op.pl>
2021-03-26 14:39:04 +0100

    User list and user list item template added

---
+++ src/data/users.js
@@ -0,0 +1,26 @@
+export const users = [
+  {
+    name: 'Alan',
+    attendance: '39%',
+    average: '2.3',
+  },
+  {
+    name: 'Fakir',
+    attendance: '30%',
+    average: '2.5',
+  },
+  {
+    name: 'Seba',
+    attendance: '90%',
+    average: '4.3',
+  },
+  {
+    name: 'Reksio',
+    attendance: '40%',
+    average: '4.6',
+  },
+  {
+    name: 'Bonifacy',
+    average: '4.6',
+  },
+];
